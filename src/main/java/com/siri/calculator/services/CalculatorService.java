8ae0f97cb8c8bc15ac84d78ca6494243424d5f2d
Jonathan Siri <jsiri@highroads.com>
2021-11-21 11:56:26 +0000

    remove unused imports and tidied up

--- src/main/java/com/siri/calculator/services/CalculatorService.java
+++ src/main/java/com/siri/calculator/services/CalculatorService.java
@@ -2,12 +2,7 @@ package com.siri.calculator.services;
 
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
-import net.objecthunter.exp4j.ValidationResult;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CalculatorService {
@@ -23,8 +18,7 @@ public class CalculatorService {
 					.variable(calculation)
 					.build();
 
-			double res = e.evaluate();
-			return res;
+			return e.evaluate();
 
 	}
 }
